1c656b82697f315a93fbe6c4e4aa9c978f134ec9
Daniel Saavedra <danielsaavedram@hotmail.com>
2017-01-09 19:21:04 -0500

    Progress: currently obtaining user coordinates. Missing using then on an API call

--- Weather App/js/script.js
+++ Weather App/js/script.js
@@ -1,3 +1,21 @@
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(assignPossition);
+        console.log("Yass");
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+    }
+}
+
+function assignPossition(position) {
+  var coords = {
+    lat:position.coords.latitude,
+    long:position.coords.longitude
+  };
+  $(".info").html("Latitude: " + coords.lat);
+}
+
 $(document).ready(function(){
-  $(".info").html("Hello");
+  // $(".info").html("Hello");
+  getLocation();
 });
