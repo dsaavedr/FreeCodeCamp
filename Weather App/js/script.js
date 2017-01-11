a8b02be79fe0905acd20133190675aca14d77903
Daniel Saavedra <danielsaavedram@hotmail.com>
2017-01-10 21:12:38 -0500

    Functionality test

--- Weather App/js/script.js
+++ Weather App/js/script.js
@@ -1,3 +1,6 @@
+var key = "234b6aedce614c99b89175711498ee6b"
+
+
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(assignPossition);
@@ -13,6 +16,19 @@ function assignPossition(position) {
     long:position.coords.longitude
   };
   $(".info").html("Latitude: " + coords.lat + "<br>" + "Longitude: " + coords.long);
+  $.getJSON('https://api.darksky.net/forecast/' + key + "/" + coords.lat + "," + coords.long, function(data) {
+    var items = [],
+                $ul;
+
+            $.each(data, function (key, val) {
+                //iterate through the returned data and build a list
+                items.push('<li id="' + key + '"><span class="name">' + val.entityname + '</span><br><span class="addr">' + val.principaladdress1 + '</span> <span class="city">' + val.principalcity + '</span></li>');
+            });
+            $ul = $('<ul />').appendTo('.info');
+
+                    //append list items to list
+                    $ul.append(items);
+  });
 }
 
 $(document).ready(function(){
