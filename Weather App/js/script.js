be2edb0b53daa0bd66dd7d9c9d24cffb91df9a62
Daniel Saavedra <danielsaavedram@hotmail.com>
2017-01-12 23:26:47 -0500

    Bootstrap and weather icons added, beginning CSS

--- Weather App/js/script.js
+++ Weather App/js/script.js
@@ -4,7 +4,7 @@ var key = "234b6aedce614c99b89175711498ee6b"
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(assignPossition);
-        console.log("Yass");
+        // console.log("Yass");
     } else {
         console.log("Geolocation is not supported by this browser.");
     }
@@ -15,7 +15,6 @@ function assignPossition(position) {
     lat:position.coords.latitude,
     long:position.coords.longitude
   };
-  $(".info").html("Latitude: " + coords.lat + "<br>" + "Longitude: " + coords.long);
   // $.getJSON('https://api.darksky.net/forecast/' + key + "/" + coords.lat + "," + coords.long, function(data) {
   //   var items = [],
   //               $ul;
@@ -33,8 +32,37 @@ function assignPossition(position) {
     url: "https://api.forecast.io/forecast/" + key + "/" + coords.lat + "," + coords.long,
     dataType: "jsonp",
     success: function (data) {
-        console.log('here');
+        // console.log('here');
         console.log(data);
+        var icon=data.currently.icon
+
+        console.log(icon=="partly-cloudy-night");
+
+        $("#c-icon").removeClass();
+
+        if (icon=="clear-day") {
+          $("#c-icon").addClass("wi wi-day-sunny");
+        } else if (icon=="clear-night") {
+          $("#c-icon").addClass("wi wi-night-clear");
+        } else if (icon=="partly-cloudy-day") {
+          $("#c-icon").addClass("wi wi-day-cloudy");
+        } else if (icon=="partly-cloudy-night") {
+          $("#c-icon").addClass("wi wi-night-cloudy");
+        } else if (icon=="cloudy") {
+          $("#c-icon").addClass("wi wi-cloudy");
+        } else if (icon=="rain") {
+          $("#c-icon").addClass("wi wi-rain");
+        } else if (icon=="sleet") {
+          $("#c-icon").addClass("wi wi-sleet");
+        } else if (icon=="snow") {
+          $("#c-icon").addClass("wi wi-snow");
+        } else if (icon=="wind") {
+          $("#c-icon").addClass("wi wi-windy");
+        } else if (icon=="fog") {
+          $("#c-icon").addClass("wi wi-fog");
+        }
+
+        $("body").append("<div>Hello</div>" + icon);
     }
   });
 }
@@ -43,3 +71,16 @@ $(document).ready(function(){
   // $(".info").html("Hello");
   getLocation();
 });
+
+// Possible values for icon on Dark Sky API
+
+// clear-day
+// clear-night
+// partly-cloudy-day
+// partly-cloudy-night
+// cloudy
+// rain
+// sleet
+// snow
+// wind
+// fog
