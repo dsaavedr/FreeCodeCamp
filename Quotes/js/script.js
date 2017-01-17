cf3b9633cca7fb264ee149f8f395df8f5db92058
Daniel Saavedra <danielsaavedram@hotmail.com>
2017-01-17 16:56:12 -0500

    CSS and Javascript added to app

---
+++ Quotes/js/script.js
@@ -0,0 +1,70 @@
+var quotes = {
+  id : {
+    1: {quote: "The world is full of people whose notion of a satisfactory future is, in fact, a return to the idealised past.",
+
+    author: "Robertson Davies, 'A Voice from the Attic', 1960",
+    },
+    2: {
+    quote: "The thing that I'm always left with is this overwhelming desire for people to be rooted and the only way that they feel rooted is through another person.",
+
+    author: "John Allen, A network called 'Internet', CBC, 10-08-93",
+    },
+    3: {
+    quote: "He can make me love something just by showing me the energy with which he loves it.",
+
+    author: "Heather Armstrong, Dooce Plugs In, 07-12-06",
+    },
+    4: {
+    quote: "The more you know, the less you need.",
+
+    author: "Aboriginal Saying",
+    },
+    5: {
+    quote: "A witty saying proves nothing.",
+
+    author: "Voltaire (1694 - 1778)",
+    },
+    6: {
+    quote: "'Never put off until tomorrow what you can do today.' Under the influence of this pestilent morality, I am forever letting tomorrow's work slop backwards into today's, and doing painfully and nervously today what I could do quickly and easily tomorrow.",
+
+    author: "J. A. Spender",
+    },
+    7: {
+    quote: "I am never afraid of what I know.",
+
+    author: "Anna Sewell (1820 - 1878)",
+    },
+    8: {
+    quote: "I'm too shy to express my sexual needs except over the phone to people I don't know.",
+
+    author: "Garry Shandling (1949 - )",
+    },
+    9: {
+    quote: "When I was younger I thought success was something different. I thought, ' When I grow up, I want to be famous. I want to be a star. I want to be in movies. When I grow up I want to see the world, drive nice cars. I want to have groupies.' But my idea of success is different today. For me, the most important thing in your life is to live your life with integrity and not to give into peer pressure, to try to be something that you're not. To live your life as an honest and compassionate person. To contribute in some way.",
+
+    author: "Ellen DeGeneres, Tulane Commencement Speech, 2009",
+    },
+    10: {
+    quote: "Everything has its wonders, even darkness and silence, and I learn whatever state I am in, therin to be content.",
+
+    author: "Helen Keller (1880 - 1968)",
+    }
+  }
+}
+
+var quoteNum = 0;
+
+$(document).ready(function () {
+  quoteNum = Math.floor(Math.random()*10+1);
+  $(".quote").html(quotes["id"][quoteNum]["quote"]);
+  $(".author").html("- " + quotes["id"][quoteNum]["author"]);
+  $('.twitter-link').attr('href', 'https://twitter.com/intent/tweet?text=' + quotes["id"][quoteNum]["quote"] + " - " + quotes["id"][quoteNum]["author"]);
+  $(".col-sm-4 .quote-btn").on("click", function () {
+    var quoteNum = 0;
+    quoteNum = Math.floor(Math.random()*10+1);
+    $(".quote").html(quotes["id"][quoteNum]["quote"]);
+    $(".author").html("- " + quotes["id"][quoteNum]["author"]);
+    $('.twitter-link').attr('href', 'https://twitter.com/intent/tweet?text=' + quotes["id"][quoteNum]["quote"] + " - " + quotes["id"][quoteNum]["author"]
+                           );
+  });
+});
