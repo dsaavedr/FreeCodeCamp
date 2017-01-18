e8c6b6abe9b281de8bc613163b48ded00aeff638
Daniel Saavedra <danielsaavedram@hotmail.com>
2017-01-17 23:36:19 -0500

    https added to API request

--- Wiki-viewer/js/script.js
+++ Wiki-viewer/js/script.js
@@ -2,7 +2,7 @@ search = function() {
   var val = $("input").value;
 
   $.ajax({
-    url: "http://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=" + val,
+    url: "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=" + val,
     dataType: "json",
     success: function (data) {
       console.log(data);
