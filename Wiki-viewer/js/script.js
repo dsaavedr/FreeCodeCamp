56977a5cfb916e5c2b79617e4c3dacfd99061724
Daniel Saavedra <danielsaavedram@hotmail.com>
2017-01-18 01:03:04 -0500

    Completely functional app with full requirements

--- Wiki-viewer/js/script.js
+++ Wiki-viewer/js/script.js
@@ -1,12 +1,38 @@
 search = function() {
-  var val = $("input").value;
+
+  $(".content").html(" ");
+
+  var val = $("#search")[0].value;
 
   $.ajax({
     url: "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=" + val,
-    dataType: "json",
+    dataType: "jsonp",
     success: function (data) {
       console.log(data);
+
+      result = data.query.pages;
+
+      for (var i in result) {
+        $("<a target='_blank' href='https://en.wikipedia.org/wiki/" + result[i].title.split(" ").join("_") +
+        "'><div class='item col-lg-3'>" +
+        "<h3>" + result[i].title + "</h3>" +
+        "<p>" + result[i].extract +
+        "</div></a>")
+          .appendTo(".content");
+      }
     }
   });
 
 }
+
+$(document).ready(function() {
+    $(function () {
+      $('[data-toggle="tooltip"]').tooltip()
+    })
+
+    $("#search").keypress(function(event) {
+    if (event.which == 13) {
+        search();
+    }
+});
+});
