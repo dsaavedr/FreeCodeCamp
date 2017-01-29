9ba10f1315cff10bd5891e0ed92ac2522934e70e
Daniel Saavedra <danielsaavedram@hotmail.com>
2017-01-28 22:03:44 -0500

    TwitchTV applet - initial commit

---
+++ twitchAPI/js/script.js
@@ -0,0 +1,30 @@
+var url = "https://wind-bow.gomix.me/twitch-api/"
+
+$(document).ready(function() {
+  $.ajax({
+    url: url + 'streams/freecodecamp',
+    dataType: 'jsonp',
+    success: function (data) {
+
+      console.log(data);
+
+      if (data.stream===null) {
+        var link=data._links.channel;
+        link=link.split("/");
+        link=link[link.length - 1];
+        $.ajax({
+          url: url + "channels/" + link,
+          dataType: "jsonp",
+          success: function(data) {
+            console.log(data);
+            window.channel = data
+          }
+        });
+      } else {
+
+      }
+
+    }
+
+  });
+});
