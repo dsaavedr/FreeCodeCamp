79ff188fe5003f784f279959d5a35237045473f8
Daniel Saavedra <danielsaavedram@hotmail.com>
2017-01-30 06:49:15 -0500

    Array of channels and Ajax loop added

--- twitchAPI/js/script.js
+++ twitchAPI/js/script.js
@@ -1,30 +1,33 @@
 var url = "https://wind-bow.gomix.me/twitch-api/"
+var channels = ["MedryBW", "ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
 $(document).ready(function() {
-  $.ajax({
-    url: url + 'streams/MedryBW',
-    dataType: 'jsonp',
-    success: function (data) {
+  for (i=0; i<channels.length; i++) {
+    $.ajax({
+      url: url + 'streams/' + channels[i],
+      dataType: 'jsonp',
+      success: function (data) {
 
-      console.log(data);
+        console.log(data);
 
-      if (data.stream===null) {
-        var link=data._links.channel;
-        link=link.split("/");
-        link=link[link.length - 1];
-        $.ajax({
-          url: url + "channels/" + link,
-          dataType: "jsonp",
-          success: function(data) {
-            console.log(data);
-            window.channel = data;
-          }
-        });
-      } else {
-        window.channel = data.stream.channel;
+        if (data.stream===null) {
+          var link=data._links.channel;
+          link=link.split("/");
+          link=link[link.length - 1];
+          $.ajax({
+            url: url + "channels/" + link,
+            dataType: "jsonp",
+            success: function(data) {
+              console.log(data);
+              window.channel = data;
+            }
+          });
+        } else {
+          window.channel = data.stream.channel;
+        }
+
       }
 
-    }
-
-  });
+    });
+  }
 });
