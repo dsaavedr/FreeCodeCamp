5bb8746eaf65dd642e2836032e7d04cd76e8405b
Daniel Saavedra <danielsaavedram@hotmail.com>
2017-02-09 11:43:50 -0500

    Info objects managed and divs created for info to be filled in

--- twitchAPI/js/script.js
+++ twitchAPI/js/script.js
@@ -1,5 +1,6 @@
 var url = "https://wind-bow.gomix.me/twitch-api/"
 var channels = ["MedryBW", "ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+var info = {};
 
 $(document).ready(function() {
   for (i=0; i<channels.length; i++) {
@@ -19,11 +20,12 @@ $(document).ready(function() {
             dataType: "jsonp",
             success: function(data) {
               console.log(data);
-              window.channel = data;
+              addBox(data);
             }
+
           });
         } else {
-          window.channel = data.stream.channel;
+          addBox(data.stream.channel);
         }
 
       }
@@ -31,3 +33,9 @@ $(document).ready(function() {
     });
   }
 });
+
+addBox = function(d) {
+  var c = $("#content")[0];
+
+  c.innerHTML+="<div class='channel'>hello</div>";
+}
