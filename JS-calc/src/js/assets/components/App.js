bbd7c098bfda75da26f5fb9cef8a24aeae8aa54c
Daniel Saavedra <danielsaavedram@hotmail.com>
2017-05-22 11:31:09 -0500

    React properties and prop-types checking created

--- JS-calc/src/js/assets/components/App.js
+++ JS-calc/src/js/assets/components/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+const Headline = () => {
+        return <h2 className="text-center">Subtitle created with react</h2>
+      },
+      Greeting = ({ name, age }) => ( // Destructuring props object
+        <p className="text-primary text-center">Testing rendering with JSX - welcome to the react world, {name}, you {age} year old bastard!</p> // Implicit return
+      )
 
 export const App = () => {
   return (
     <div>
-      <h2 className="text-center">Subtitle created with react</h2>
-      <p className="text-primary text-center">Testing rendering with JSX</p>
+      <Headline />
+      <Greeting name="Daniel" age={20} />
     </div>
   )
 };
@@ -12,3 +20,8 @@ export const App = () => {
 export const SecondApp = () => {
   return <h1>TEST</h1>
 };
+
+Greeting.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
+}
--- JS-calc/src/js/assets/react-test.js
+++ JS-calc/src/js/assets/react-test.js
@@ -3,9 +3,6 @@ import { render } from 'react-dom';
 import { App, SecondApp } from './components/App.js';
 
 render(
-  <div>
-    <App />
-    <SecondApp />
-  </div>,
+<App />,
 document.getElementById('react-root')
 );
