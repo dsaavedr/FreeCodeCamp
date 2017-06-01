7cbea18ff4d140982b960cf48565c1e6dc7dde7b
Daniel Saavedra <danielsaavedram@hotmail.com>
2017-06-01 04:59:06 -0500

    Shopping list created with React

--- JS-calc/src/js/assets/components/App.js
+++ JS-calc/src/js/assets/components/App.js
@@ -1,27 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Headline = () => {
-        return <h2 className="text-center">Subtitle created with react</h2>
-      },
-      Greeting = ({ name, age }) => ( // Destructuring props object
-        <p className="text-primary text-center">Testing rendering with JSX - welcome to the react world, {name}, you {age} year old bastard!</p> // Implicit return
-      )
+// const Headline = () => {
+//         return <h2 className="text-center">Subtitle created with react</h2>
+//       },
+//       Greeting = ({ name, age }) => ( // Destructuring props object
+//         <p className="text-primary text-center">Testing rendering with JSX - welcome to the react world, {name}, you {age} year old bastard!</p> // Implicit return
+//       )
 
-export const App = () => {
-  return (
-    <div>
-      <Headline />
-      <Greeting name="Daniel" age={20} />
-    </div>
-  )
-};
+export class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      pendingItems: ['veggies', 'bread', 'fruits', 'bananas'],
+
+    }
+  }
+  render(){
+    const { pendingItems } = this.state;
+    return (
+      <div>
+         <h1>Shopping list</h1>
+         <ul>
+           {
+             pendingItems.map((item, key) => {
+               return <li key={key}>{item}</li>
+             })
+           }
+         </ul>
+      </div>
+    )
+  }
+}
+
+// export const App = () => {
+// };
 
 export const SecondApp = () => {
   return <h1>TEST</h1>
 };
-
-Greeting.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
-}
+//
+// Greeting.propTypes = {
+//   name: PropTypes.string,
+//   age: PropTypes.number.isRequired
+// }
