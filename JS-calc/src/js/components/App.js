9f985519d476e9b42df1e706f32a480510df7b07
Daniel Saavedra <danielsaavedram@hotmail.com>
2017-06-06 09:21:41 -0500

    Project started. React HTML and CSS started

--- JS-calc/src/js/components/App.js
+++ JS-calc/src/js/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import PropTypes from 'prop-types';
-import cli from './assets/evaluate';
+import cli from '../assets/evaluate';
 
 export class App extends React.Component {
   constructor(props){
@@ -13,45 +13,42 @@ export class App extends React.Component {
   }
 
   render(){
-    const { listItems } = this.state;
     return (
-      <div id="calculator">
-        <div id="display"></div>
+      <div id="calculator" className="container">
+        <div id="display">0</div>
         <div id="keypad">
-          <div id="functions">
-            <button type="button" className="AC">AC</button>
-            <button type="button" className="(-)">(-)</button>
-            <button type="button" className="%">%</button>
-          </div>
-          <div id="numbers">
-            <button type="button" className="0">0</button>
-            <button type="button" className="1">1</button>
-            <button type="button" className="2">2</button>
-            <button type="button" className="3">3</button>
-            <button type="button" className="4">4</button>
-            <button type="button" className="5">5</button>
-            <button type="button" className="6">6</button>
-            <button type="button" className="7">7</button>
-            <button type="button" className="8">8</button>
-            <button type="button" className="9">9</button>
-          </div>
-          <div id="operators">
-            <button type="button">/</button>
-            <button type="button">x</button>
-            <button type="button">-</button>
-            <button type="button">+</button>
-            <button type="button">=</button>
+          <div id="keys">
+            <div id="functions">
+              <button type="button" className="btn func-AC">AC</button>
+              <button type="button" className="btn func-(-)">(-)</button>
+              <button type="button" className="btn func-%">%</button>
+            </div>
+            <div id="numbers">
+              <button type="button" className="btn num-0">0</button>
+              <button type="button" className="btn num-.">.</button>
+              <button type="button" className="btn num-1">1</button>
+              <button type="button" className="btn num-2">2</button>
+              <button type="button" className="btn num-3">3</button>
+              <button type="button" className="btn num-4">4</button>
+              <button type="button" className="btn num-5">5</button>
+              <button type="button" className="btn num-6">6</button>
+              <button type="button" className="btn num-7">7</button>
+              <button type="button" className="btn num-8">8</button>
+              <button type="button" className="btn num-9">9</button>
+            </div>
+            <div id="operators">
+              <button type="button" className="btn op-/">/</button>
+              <button type="button" className="btn op-x">x</button>
+              <button type="button" className="btn op--">-</button>
+              <button type="button" className="btn op-+">+</button>
+              <button type="button" className="btn op-=">=</button>
+            </div>
           </div>
         </div>
       </div>
     )
-    //  <button type="button" className="close" aria-label="close"><span aria-hidden="true">&times;</span></button>
   }
 }
-
-export const SecondApp = () => {
-  return <h1>TEST</h1>
-};
 //
 // Greeting.propTypes = {
 //   name: PropTypes.string,
