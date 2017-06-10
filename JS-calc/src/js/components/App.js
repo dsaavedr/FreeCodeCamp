da3aa1f9838a00a8a644a57a01f47e4ba219e16b
Daniel Saavedra <danielsaavedram@hotmail.com>
2017-06-10 17:53:33 -0500

    Implementation of example CSS finished. NOT WORKING

--- JS-calc/src/js/components/App.js
+++ JS-calc/src/js/components/App.js
@@ -19,29 +19,29 @@ export class App extends React.Component {
         <div id="keypad">
           <div id="keys">
             <div id="functions">
-              <button type="button" className="btn func-AC">AC</button>
-              <button type="button" className="btn func-(-)">(-)</button>
-              <button type="button" className="btn func-%">%</button>
+              <button type="button" className="btn key func-AC">AC</button>
+              <button type="button" className="btn key func-(-)">(-)</button>
+              <button type="button" className="btn key func-%">%</button>
             </div>
             <div id="numbers">
-              <button type="button" className="btn num-0">0</button>
-              <button type="button" className="btn num-.">.</button>
-              <button type="button" className="btn num-1">1</button>
-              <button type="button" className="btn num-2">2</button>
-              <button type="button" className="btn num-3">3</button>
-              <button type="button" className="btn num-4">4</button>
-              <button type="button" className="btn num-5">5</button>
-              <button type="button" className="btn num-6">6</button>
-              <button type="button" className="btn num-7">7</button>
-              <button type="button" className="btn num-8">8</button>
-              <button type="button" className="btn num-9">9</button>
+              <button type="button" className="btn key num-0">0</button>
+              <button type="button" className="btn key num-.">.</button>
+              <button type="button" className="btn key num-1">1</button>
+              <button type="button" className="btn key num-2">2</button>
+              <button type="button" className="btn key num-3">3</button>
+              <button type="button" className="btn key num-4">4</button>
+              <button type="button" className="btn key num-5">5</button>
+              <button type="button" className="btn key num-6">6</button>
+              <button type="button" className="btn key num-7">7</button>
+              <button type="button" className="btn key num-8">8</button>
+              <button type="button" className="btn key num-9">9</button>
             </div>
             <div id="operators">
-              <button type="button" className="btn op-/">/</button>
-              <button type="button" className="btn op-x">x</button>
-              <button type="button" className="btn op--">-</button>
-              <button type="button" className="btn op-+">+</button>
-              <button type="button" className="btn op-=">=</button>
+              <button type="button" className="btn key op-/">/</button>
+              <button type="button" className="btn key op-x">x</button>
+              <button type="button" className="btn key op--">-</button>
+              <button type="button" className="btn key op-+">+</button>
+              <button type="button" className="btn key op-=">=</button>
             </div>
           </div>
         </div>
